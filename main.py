102c5864f60b14d5b8996408a884c4c9e4faa853
harsh_jain <harshjniitr@gmail.com>
2017-06-17 20:19:52 +0530

    made it object oriented

--- main.py
+++ main.py
@@ -1,32 +1,14 @@
 import re
-
+"""
 
 def filtering(message): 
+	message=message.lower()
 	mes_split=re.findall("(\d|\w)+",message)	
-	sen_type=0	
-	a_the=[r"a",r"t+h+e+",r"d"]	
-	intr_set=set()
-	intrjectve_hap=[r"h+u+r+(a+|e+)y+",r"b+r+(a+|e+)+v+o+",r"(w+e+l+)\s*(d+o*n+e*)"]
-	intrjectve_amze=[r"w+o+w+",r"h+e+y+",r"h+a+y+",r"(o+h+)\s+(m+y+)\s+(g+o*s+h+)"]
-	intrjectve_sd=[r"o+p+s+",r"o+u+c+h+",r"a+l+a*s+",r"o+h+\s*,*\s*n+o+"]
-	intr_type=0	
 
-	greeting=[r"h+i+",r"h+e*l+o+",r"y+o+",r"y+u+p+"]
-	command=[r""]
-	request=[r"p+l+z+s*",r"p+l+e*a*s+e*"]	
 
-	conj_cor=[r"a+n+d+",r"o+r+"]
-	nei_ei=[r"n+e*i*t+h*e*r+",r"e+i*t+h+e*r+",r"n+o*r+",r"o+r+"]
-	conj_sub=[r"b+e*a*c+a*u+s+e+",r"s+o+",r"t+h+e+n+",r"w+h+i+l+e+",r"b+u+t+"]
-	and_or=set()
-	sen_break=set()	
 
 	
 
-	pro_time=[r"o+n+",r"a+t+",r"i+n+"]
-	prepostion=[r"w+i+t+h+",r"f+r+o+m+",r"i+n+t+o+",r"d+u+r+i*n+g+",r"i+n+c+l+u*d+i*n+g+",r"u+n+t+i*l+",r"a+g+a*i*n+s*t+",r"a+m+o*n+g+",r"t+h+r+o*u*g+h+",r"t+h+r+o*u*g+h+o+u*t+"]
-	pro_verb=[r"o+f+",r"t+o+",r"i+n+",r"f+o+r+",r"o+n+",r"b+y+",r"a+b+o*u*t+",r"l+i*k+e+",r"o+v+e*r+",r"b+e+f+o+r+e+",r"b+e*t+w+e+n+",r"a+f+t+e*r+",r"s+i+n+c+e+",r"w+i+t+h+o*u*t+"]
-	pro_verb2=[r"w+i+t+h*i*n+"]	
 
 
 	pron=[r"i+", r"i+t+", r"h+e+", r"s+h+e+",  r"w+e+", r"t+h+e+y+"]
@@ -36,50 +18,100 @@ def filtering(message):
 
 	
 
-	if '!' in message:
-		sen_type=3
-	elif '?' in message:
-		sen_type=2
-	else: 
-		sen_type=1	
+
 
 	
 
-	mes_tmp=mes_split[:]
-	for i in mes_tmp:
-		for j in a_the:
-			if re.search(r"\s"+j+r"\s"," "+i+" "):
-				mes_split.remove(i)
+	greeting=[r"h+i+",r"h+e*l+o+",r"y+o+",r"y+u+p+"]
+	command=[r""]
+	request=[r"p+l+z+s*",r"p+l+e*a*s+e*"]	
+	
 
-	mes_tmp=mes_split[:]
-	for i in mes_tmp:
-		for j in intrjectve_hap:
-			if re.search(r"\s"+j+r"\s"," "+i+" "):
-				intr_set.add(i)
-				mes_split.remove(i)
-				intr_type+=1
-				continue
 
-		for j in intrjectve_amze:
-			if re.search(r"\s"+j+r"\s"," "+i+" "):
-				intr_set.add(i)
-				mes_split.remove(i)
-				intr_type+=.6
-				continue
-
-		for j in intrjectve_sd:
-			if re.search(r"\s"+j+r"\s"," "+i+" "):
-				intr_set.add(i)
-				mes_split.remove(i)
-				intr_type-=1
-				continue
-
-	return " ".join(mes_split)
+	"""
 
 
 
 
-
+	
 			
 
 
+	
+class filter_message:
+	
+	#article
+	a_the=[r"a",r"t+h+e+",r"d"]	
+	
+	#interjection
+	intrjectve_hap=[r"h+u+r+(a+|e+)y+",r"b+r+(a+|e+)+v+o+",r"(w+e+l+)\s*(d+o*n+e*)"]
+	intrjectve_amze=[r"w+o+w+",r"h+e+y+",r"h+a+y+",r"(o+h+)\s+(m+y+)\s+(g+o*s+h+)"]
+	intrjectve_sd=[r"o+p+s+",r"o+u+c+h+",r"a+l+a*s+",r"o+h+\s*,*\s*n+o+"]
+	
+	#pronoun
+	pro_time=[r"o+n+",r"a+t+",r"i+n+"]
+	prepostion=[r"w+i+t+h+",r"f+r+o+m+",r"i+n+t+o+",r"d+u+r+i*n+g+",r"i+n+c+l+u*d+i*n+g+",r"u+n+t+i*l+",r"a+g+a*i*n+s*t+",r"a+m+o*n+g+",r"t+h+r+o*u*g+h+",r"t+h+r+o*u*g+h+o+u*t+"]
+	pro_verb=[r"o+f+",r"t+o+",r"i+n+",r"f+o+r+",r"o+n+",r"b+y+",r"a+b+o*u*t+",r"l+i*k+e+",r"o+v+e*r+",r"b+e+f+o+r+e+",r"b+e*t+w+e+n+",r"a+f+t+e*r+",r"s+i+n+c+e+",r"w+i+t+h+o*u*t+"]
+	pro_verb2=[r"w+i+t+h*i*n+"]	
+
+
+	#conjecation
+	conj_cor=[r"a+n+d+",r"o+r+"]
+	nei_ei=[r"n+e*i*t+h*e*r+",r"e+i*t+h+e*r+",r"n+o*r+",r"o+r+"]
+	conj_sub=[r"b+e*a*c+a*u+s+e+",r"s+o+",r"t+h+e+n+",r"w+h+i+l+e+",r"b+u+t+"]
+	and_or=set()
+	sen_break=set()	
+
+
+	def __init__(self,message):
+		self.message=message.lower()
+		self.mes_split=re.findall("[(\d|\w)]+",self.message)
+		self.info={"intr_type":0,"intr_set":set()}
+		self.intr_type=0
+		self.intr_set=set()
+		self.filtering()
+		
+
+
+	def filtering(self):
+		self.sentence_type()
+		self.filter_method(filter_message.a_the)
+		self.intr_type=self.advance_filter_method({0:1,1:-1,2:.6},self.intr_set,filter_message.intrjectve_hap,filter_message.intrjectve_sd,filter_message.intrjectve_amze)
+		self.filter_method(filter_message.pro_time,filter_message.prepostion,filter_message.pro_verb,filter_message.pro_verb2)
+		self.filter_method(filter_message.conj_cor,filter_message.conj_sub,filter_message.nei_ei)
+
+
+	def filter_method(self,*filters_array):
+		mes_tmp=self.mes_split[:]
+		for i in mes_tmp:
+			for k in filters_array:
+				for j in k:
+					if re.search(r"\s"+j+r"\s"," "+i+" "):
+						self.mes_split.remove(i)
+						break
+
+	# measuring qualities
+	def advance_filter_method(self,value_set,store,*filters_array):
+		value=0
+		mes_tmp=self.mes_split[:]
+		for i in mes_tmp:
+			for k in filters_array:
+				for j in k:
+					if re.search(r"\s"+j+r"\s"," "+i+" "):
+						store.add(i)
+						self.mes_split.remove(i)
+						value+=value_set[filters_array.index(k)]
+						break
+		return value
+
+	# 2 "interogative" ,3 "excalmation" ,1 "else"
+	def sentence_type(self):
+		if '!' in self.message:
+			self.sen_type=3
+		elif '?' in self.message:
+			self.sen_type=2
+		else: 
+			self.sen_type=1
+
+	def get_filtered_message(self):
+		return " ".join(self.mes_split) ,self.intr_type,self.sen_type ,self.intr_set
